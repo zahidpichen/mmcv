39bd540264b6065958e3a0817f4eacf9c5fef572
zahidpichen <137002342+zahidpichen@users.noreply.github.com>
2025-06-18 20:20:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ try:
         from torch.utils.cpp_extension import BuildExtension
         EXT_TYPE = 'pytorch'
     cmd_class = {'build_ext': BuildExtension}
+    print(f'Successfully found CUDA, using EXT_TYPE = {EXT_TYPE}')
 except ModuleNotFoundError:
     cmd_class = {}
     print('Skip building ext ops due to the absence of torch.')
